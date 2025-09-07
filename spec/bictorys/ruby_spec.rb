3b8074eb58c11ef02e39fd0820c51f6f58c1c928
Badara <badou017@gmail.com>
2025-09-07 14:04:18 +0000

    chore: Initialize bictorys-ruby gem

---
+++ spec/bictorys/ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bictorys::Ruby do
+  it "has a version number" do
+    expect(Bictorys::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
